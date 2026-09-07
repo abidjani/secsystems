1336763e2c13477cd12079240376442e081c3a57
abid <abidjani46@yahoo.com>
2026-09-07 18:49:15 +0300

    left align hero seciton on moible

--- js/custome_script.js
+++ js/custome_script.js
@@ -1,38 +1,15 @@
 
-  $(document).ready(function () {
-      var navbar = $(".navbar");
+const navbar = document.querySelector(".navbar");
 
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 20) {
-          navbar.addClass("sticky");
-        } else {
-          navbar.removeClass("sticky");
-        }
-      });
-    });
-
-$(document).ready(function(){
-  $('#logos_slider').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrows:false,
-    autoplaySpeed: 1000, // Set a lower value for continuous scrolling
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 4,
-        }
-      }
-    ]
-  });
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 80) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
 });
 
 
 
 
 
-var currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerText = currentYear;
