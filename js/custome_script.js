753f51f1400bd1c07d9b98c18524c4dd7b3aa33f
abid <abidjani46@yahoo.com>
2026-09-11 14:49:29 +0300

    slider and description row

--- js/custome_script.js
+++ js/custome_script.js
@@ -8,8 +8,50 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-scrolled");
   }
 });
+$('.actual_slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 400,
 
+    slidesToShow: 2,
+    slidesToScroll: 1,
 
+    centerMode: true,
+    centerPadding: '20px',
+    prevArrow: $('.slider_prev'),
+  nextArrow: $('.slider_next'),
+  
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: '50px'
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: '45px'
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: '25px'
+            }
+        }
+    ]
+});
 
 
 
